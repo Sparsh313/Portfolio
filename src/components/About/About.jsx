eccfdc4ec3e21eccbd41335236e9130fca10db8b
Sparsh Singh <sparshsingh03012003@gmail.com>
2025-05-19 22:18:00 +0530

    Resume updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1q7Cn87IatxMkEbu8MgNNFJmoAWIEZfmM/view?usp=sharing"
+            href="https://drive.google.com/file/d/1QZ6maGvHT-qn0hqM2hbXAlGBbKVWUdU4/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
